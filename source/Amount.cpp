da2cc7cd15d1d60a13c3da0e6184940c4861981c
Le Quang Nhat <lequangnhat0978@gmail.com>
2025-12-21 23:58:18 +0700

    Fix conflict

--- source/Amount.cpp
+++ source/Amount.cpp
@@ -1,7 +1,8 @@
-#include "C:\Users\Admin\Documents\GitHub\Endterm_Project_Term1_CS160\header\Amount.h"
+/*#include "C:\Users\Admin\Documents\GitHub\Endterm_Project_Term1_CS160\header\Amount.h"
 
 void Input(string info)
 {
     stringstream ss(info);
-    
+
 }
+    */
